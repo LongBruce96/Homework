d1486d457e60a333544a36bde1e17b2ac4a5fd38
LongBruce96 <48952658+LongBruce96@users.noreply.github.com>
2019-04-16 18:09:32 +0700

    Update bmi.py

--- Session 7/bmi.py
+++ Session 7/bmi.py
@@ -1,28 +1,3 @@
-# from math import sqrt
-
-# from flask import Flask, request
-
-# app = Flask(__name__)
-# @app.route('/bmi/<weight>/<height>')
-# def index(weight, height):
-#     # weight = request.args.get('weight', type=float)
-#     # height = request.args.get('height', type=float)
-#     bmi = int(weight / (height/100)**2)
-    
-#     if bmi < 16:
-#         return "you're: Severely Underweight"
-#     elif bmi < 18.5:
-#         return "you're: Underweight"
-#     elif bmi < 25:
-#         return "you're: Normal"
-#     elif bmis < 30:
-#         return "you're: Overweight"
-#     else:
-#         return "you're: Obese"
-
-# if __name__ == '__main__':
-#   app.run(debug=True)
-
 from math import sqrt
 
 from flask import Flask, request
@@ -31,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/bmi/<int:weight>/<int:height>')
 def index(weight, height):
-    # weight = request.args.get('weight', type=float)
-    # height = request.args.get('height', type=float)
+    
         bmi = int(weight / (height/100)**2)
         # return "Your BMI is:" + str(bmi)
 
